e50a686f5b9ae514327ab7d4a8d2c3e337d62e7f
Oladipo Isaac <oladipoolayinka421@gmail.com>
2025-09-05 17:17:05 +0100

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,4 +1,7 @@
 @echo off
+set /p url="Enter image URL: "
+
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%TEMP%\pload\GlitchGhost-main\image.png'"
 
 :: Set the wallpaper in the registry
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%TEMP%\pload\GlitchGhost-main\image.png" /f
